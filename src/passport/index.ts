2e8dd6f57cc503c5b5bc582b2af16abb6eb849f7
Witchayut <skpepe134@gmail.com>
2018-11-08 22:17:25 +0700

    add serialize to support session

--- src/passport/index.ts
+++ src/passport/index.ts
@@ -4,6 +4,14 @@ import spotify from './strategy/spotify';
 export enum AuthenticationType {
   spotify,
 }
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
 passport.use(spotify);
 
 export default passport;
